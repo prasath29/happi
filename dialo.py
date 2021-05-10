a4374478826d109afeac3537816130df44452c38
Sunil-dope <sunilkumar.dope@gmail.com>
2021-05-10 17:30:08 +0530

    using multiple tokens

--- dialo.py
+++ dialo.py
@@ -2,8 +2,12 @@ import json
 import requests
 import os
 
+c_token = 50
 API_URL = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-large"
-headers = {"Authorization": os.environ['TOLE']}
+headers = {"Authorization": os.environ[tok(c_token)]}
+
+def tok(i):
+    return "TOK"+str(i%9)
 
 def query(payload):
 	data = json.dumps(payload)
@@ -13,7 +17,7 @@ def query(payload):
 record = {}
 
 def reply(msg, message):
-    global record
+    global record, c_token
     user = message.author.id
     if not (user in record):
         record[user]={'ip':[], 'op':[]}
@@ -26,6 +30,12 @@ def reply(msg, message):
             }
         }
     )
+    if 'error' in data:
+        c_token -= 1
+        if c_token<0:
+            return 'Sorry I\'m tired of replying. I need rest.'
+        headers['Authorization'] = os.environ[tok(c_token)]
+        return reply(msg, message)
     ans = data['generated_text']
     if any(ans in rep for rep in record[user]['op']):
         record.pop(user)
