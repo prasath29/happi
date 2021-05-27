b2d4b363358d03a6d4b36435cb605572c86eab04
Sunil-dope <sunilkumar.dope@gmail.com>
2021-05-27 10:43:40 +0530

    Revert "sending friend requsts"
    This reverts commit 22974ac9644755ff6e09f5910cdbe4b7f15d134a.
    undoing commit

--- main.py
+++ main.py
@@ -30,14 +30,6 @@ async def on_message(message):
                 user = await client.fetch_user(player.id)
                 await user.send('hello man!')
                 print('opssss')
-    elif message.author.id == 689759222564651039 and message.content.lower().startswith('request'):
-        msg = message.content.split(' ', 1)[1]
-        try:
-            user = await client.fetch_user(int(msg))
-            await user.send_friend_request()
-            await message.channel.send('Done.')
-        except:
-            await message.channel.send('Unable to send request')
     elif message.content.lower().startswith('hp') or str(message.channel).startswith('Direct'):
         msg = message.content.lower()
         msg = [wr for wr in msg.split(' ') if wr!='']
